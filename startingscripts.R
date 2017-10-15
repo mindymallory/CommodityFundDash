9295f6e3f92ddde33de99a223c313c8acff3c689
mindymallory <mallorym@illinois.edu>
2017-10-15 14:42:12 -0500

    xczs

---
+++ startingscripts.R
@@ -0,0 +1,18 @@
+library(XML)
+library(RCurl)
+
+library("methods")
+
+# Convert the input xml file to a data frame.
+
+xml.URL <- 'https://www.usda.gov/oce/commodity/wasde/latest.xml'
+xmlfile <- getURL(xml.URL, ssl.verifyPeer=FALSE)
+download.file(xml.URL, destfile='latestwasde.xml' <- tempfile(fileext=".xml"))
+temp <- xmlTreeParse(latestwasde)
+
+xmldataframe <- xmlToDataFrame(temp)
+print(xmldataframe)
+
+# doesn't work but this stack overflow might hlep. 
+
+https://stackoverflow.com/questions/31732680/parsing-xml-in-r-data-stored-in-attributes-with-blanks 
